e0779c18595dc92db9a55a29226f54e63cd580c1
vivek <viveksharmapoudel@gmail.com>
2022-10-17 14:01:20 +0545

    fix eslint

--- src/utils.js
+++ src/utils.js
@@ -1,67 +1,77 @@
-import { encodeAddress, decodeAddress, blake2AsHex } from '@polkadot/util-crypto';
-import { Buffer } from 'buffer';
+import {
+	encodeAddress,
+	decodeAddress,
+	blake2AsHex,
+} from "@polkadot/util-crypto";
+import { Buffer } from "buffer";
 
 export const ADDRESS_FORMAT = {
-  ss58: 'SS58',
-  h160: 'H160',
-  snow: 'SNOW',
-  pubKey: 'Public Key',
+	ss58: "SS58",
+	h160: "H160",
+	snow: "SNOW",
+	pubKey: "Public Key",
 };
 
 export const CHAIN_PREFIX = {
-  snow: 2207,
-  ss58: 42,
+	snow: 2207,
+	ss58: 42,
 };
 
 export function convertH160ToSs58(h160Addr) {
-  validateH160(h160Addr);
-  const addressBytes = Buffer.from(h160Addr.slice(2), 'hex');
-  const prefixBytes = Buffer.from('evm:');
-  const convertBytes = Uint8Array.from(Buffer.concat([ prefixBytes, addressBytes ]));
-  const finalAddressHex = blake2AsHex(convertBytes, 256);
-  return encodeAddress(finalAddressHex, CHAIN_PREFIX.ss58);
+	validateH160(h160Addr);
+	const addressBytes = Buffer.from(h160Addr.slice(2), "hex");
+	const prefixBytes = Buffer.from("evm:");
+	const convertBytes = Uint8Array.from(
+		Buffer.concat([prefixBytes, addressBytes])
+	);
+	const finalAddressHex = blake2AsHex(convertBytes, 256);
+	return encodeAddress(finalAddressHex, CHAIN_PREFIX.ss58);
 }
 
 export function convertSs58ToH160(ss58Addr) {
-  validateSs58(ss58Addr);
+	validateSs58(ss58Addr);
 
-  const pubKey = getPubKey(ss58Addr);
-  return pubKey.slice(0, 40);
+	const pubKey = getPubKey(ss58Addr);
+	return pubKey.slice(0, 40);
 }
 
 export function getPubKey(ss58addr) {
-  return '0x' + Buffer.from(decodeAddress(ss58addr)).toString('hex');
+	return "0x" + Buffer.from(decodeAddress(ss58addr)).toString("hex");
 }
 
 export function encodePubKey(pubKey, prefix) {
-  validatePubKey(pubKey);
-  return encodeAddress(pubKey, prefix);
+	validatePubKey(pubKey);
+	return encodeAddress(pubKey, prefix);
 }
 
 export function encodePolkadotAddress(addr, prefix) {
-  try {
-    return encodeAddress(addr, prefix);
-  } catch(e) {
-    throw 'Invalid Address provided!';
-  }
+	try {
+		return encodeAddress(addr, prefix);
+	} catch (e) {
+		// eslint-disable-next-line no-throw-literal
+		throw "Invalid Address provided!";
+	}
 }
 
 function validateH160(h160Addr) {
-  const re = /0x[0-9A-Fa-f]{40}/g;
-  if(!re.test(h160Addr)) {
-    throw 'Invalid H160 address provided!';
-  } 
+	const re = /0x[0-9A-Fa-f]{40}/g;
+	if (!re.test(h160Addr)) {
+		// eslint-disable-next-line no-throw-literal
+		throw "Invalid H160 address provided!";
+	}
 }
 
 export function validateSs58(ss58Addr) {
-  if(ss58Addr.length !== 48 || ss58Addr.at(0) !== '5') {
-    throw 'Invalid SS58 address provided!';
-  }
+	if (ss58Addr.length !== 48 || ss58Addr.at(0) !== "5") {
+		// eslint-disable-next-line no-throw-literal
+		throw "Invalid SS58 address provided!";
+	}
 }
 
 export function validatePubKey(pubkey) {
-  const re = /0x[0-9a-fA-F]{64}/;
-  if(!re.test(pubkey)) {
-    throw 'Invalid Public Key provided!';
-  }
+	const re = /0x[0-9a-fA-F]{64}/;
+	if (!re.test(pubkey)) {
+		// eslint-disable-next-line no-throw-literal
+		throw "Invalid Public Key provided!";
+	}
 }
